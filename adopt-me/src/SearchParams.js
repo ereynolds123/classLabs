ba57f57f01fc863a27305ff0665b44884944ec77
Emily Reynolds <emilyreynoldsdsa@gmail.comgit config --global user.name Emily>
2020-08-26 16:24:48 -0400

    Almost working breeds, but broken.

--- adopt-me/src/SearchParams.js
+++ adopt-me/src/SearchParams.js
@@ -1,12 +1,21 @@
-import React, {useState} from "react";
-import {ANIMALS} from "@frontendmasters/pet";
+import React, {useState, useEffect} from "react";
+import pet, {ANIMALS} from "@frontendmasters/pet";
 import useDropdown from "./useDropdown"
 
 const SearchParams=()=>{
     const [location, setLocation]= useState("Seattle, WA");
+    const [breeds, setBreeds] = useState([]);
     const [animal, AnimalDropdown]= useDropdown("Animal", "dog", ANIMALS)
-    // const [breed, BreedDropdown]= useDropdown("breed", "", breeds)
+    const [breed, BreedDropdown, setBreed]= useDropdown("breed", "", breeds)
   
+    useEffect(()=>{
+        setBreeds([]);
+        setBreed("");
+        pet.breeds(animal).then(({breeds})=>{
+            const breedStrings= breed.map(({name})=>name)
+            setBreeds(breedStrings)
+        })
+    }, [animal, setBreed, setBreeds])
 
     return(
         <div className="search-params">
@@ -15,7 +24,7 @@ const SearchParams=()=>{
                 <input id="location" value={location} placeholder="location" onChange={event=>setLocation(event.target.value)}></input></label> 
                 {/* Set location updates the state, useState is the default location. This is a hook. All hooks begin with use. A hook is the current state and an updater function. hooks never go inside of if statements or for loops */}
                 <AnimalDropdown/>
-                {/* <BreedDropdown /> */}
+                <BreedDropdown />
 
                 <button>Submit</button>
             </form>
